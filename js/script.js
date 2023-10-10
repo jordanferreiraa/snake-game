281fc2c4607242381ecf51961791c00b3f7e0962
jordanferreiraa <jordanferreira69@gmail.com>
2023-10-10 11:47:40 -0300

    feat: move snake and game loop

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ const ctx = canvas.getContext("2d")
 const size = 30
 
 const snake = [
-  { x: 200, y: 200 },
-  { x: 230, y: 200 },
-  { x: 260, y: 200 },
+  { x: 270, y: 240 },
+  { x: 300, y: 240 },
+  { x: 330, y: 240 },
 ]
+
+let direction, loopId
 
 const drawSnake = () => {
   ctx.fillStyle = "white"
@@ -23,4 +25,54 @@ const drawSnake = () => {
   })
 }
 
-drawSnake()
+const moveSnake = () => {
+  if(!direction) return
+
+  //ultimo elemento
+  // const head = snake.at(-1)
+  const head = snake[snake.length - 1]
+
+  if(direction == "right") {
+    snake.push({ x: head.x + size, y: head.y })
+  }
+  if(direction == "left") {
+    snake.push({ x: head.x - size, y: head.y })
+  }
+  if(direction == "down") {
+    snake.push({ x: head.x, y: head.y + size })
+  }
+  if(direction == "up") {
+    snake.push({ x: head.x, y: head.y - size })
+  }
+
+  snake.shift()
+}
+ 
+const gameLoop = () => {
+  clearInterval(loopId)
+
+  ctx.clearRect(0, 0, 600, 600)
+  moveSnake()
+  drawSnake()
+
+  loopId = setTimeout(() => {
+    gameLoop()
+  }, 300)
+}
+
+gameLoop()
+
+document.addEventListener("keydown", (event) => {
+  if(event.key == "ArrowUp" && direction != "down") {
+    direction = "up"
+  }
+  if(event.key == "ArrowDown" && direction != "up") {
+    direction = "down"
+  }
+  if(event.key == "ArrowRight" && direction != "left") {
+    direction = "right"
+  }
+  if(event.key == "ArrowLeft" && direction != "right") {
+    direction = "left"
+  }
+})
